017a7afb593ed4125ad6d7eb9e199dccbc048a17
gabrielsimplicio00 <gaasimplicio@gmail.com>
2022-09-12 21:27:17 -0300

    added a try/catch in every method in the controller / made an error page

--- src/controllers/PessoasController.js
+++ src/controllers/PessoasController.js
@@ -5,75 +5,120 @@ const bodyParser = require('body-parser')
 class PessoaController {
 
     static exibeFormulario(req, res) {
-        return res.sendFile('index.html', {root: path.join(__dirname, '../public/html')})
-        // return res.status(200).send({mensagem: 'Welcome to the API'})
+        try {
+            return res.sendFile('index.html', {root: path.join(__dirname, '../public/html')})
+        } catch (error) {
+            res.render('erro', {
+                erro: error.message
+            })
+        }
     }
 
     static async exibePessoas(req, res) {
-        const pessoas = await Pessoas.find({})
-        res.render('listaPessoas', {
-            pessoasList: pessoas
-        })
-        // return res.status(200).send(pessoas)
+        try {
+            const pessoas = await Pessoas.find({})
+            return res.render('listaPessoas', {
+                pessoasList: pessoas
+            }) 
+        } catch (error) {
+            return res.render('erro', {
+                erro: error.message
+            })
+        }
     }
 
     static async criaPessoa(req, res) {
-        const novaPessoa = await Pessoas.create({
-            nome: req.body.nome,
-            sobrenome: req.body.sobrenome,
-            idade: req.body.idade,
-            email: req.body.email,
-            profissao: req.body.profissao
-        }) 
-        // await Pessoas.create(req.body)
-        return res.redirect('/')
+        try {
+            await Pessoas.create({
+                nome: req.body.nome,
+                sobrenome: req.body.sobrenome,
+                idade: req.body.idade,
+                email: req.body.email,
+                profissao: req.body.profissao
+            }) 
+            return res.redirect('/')
+            
+        } catch (error) {
+            return res.render('erro', {
+                erro: error.message
+            })
+        }
     }
 
     static async exibeUmaPessoa(req, res){
-        const { id } = req.params
-        const pessoa = await Pessoas.findById(id)
-        return res.render('listaUmaPessoa', {
-            umaPessoa: pessoa
-        })
+        try {
+            const { id } = req.params
+            const pessoa = await Pessoas.findById(id)
+            return res.render('listaUmaPessoa', {
+                umaPessoa: pessoa
+            })
+        } catch (error) {
+            return res.render('erro', {
+                erro: error.message
+            })
+        }
     }
 
     static async atualizaPessoaPagina(req, res) {
-        const { id } = req.params
-        const pessoa = await Pessoas.findById(id)
-        res.render('atualizaPessoaPagina', {
-            pessoa: pessoa
-        })
+        try {
+            const { id } = req.params
+            const pessoa = await Pessoas.findById(id)
+            return res.render('atualizaPessoaPagina', {
+                pessoa: pessoa
+            })
+        } catch (error) {
+            return res.render('erro', {
+                erro: error.message
+            })
+        }
     }
 
     static async atualizaPessoa(req, res) {
-        const id = req.body.id
-        const dados = {
-            id: id,
-            nome: req.body.nome,
-            sobrenome: req.body.sobrenome,
-            idade: req.body.idade,
-            email: req.body.email,
-            profissao: req.body.profissao
+        try {
+            const id = req.body.id
+            const dados = {
+                id: id,
+                nome: req.body.nome,
+                sobrenome: req.body.sobrenome,
+                idade: req.body.idade,
+                email: req.body.email,
+                profissao: req.body.profissao
+            }
+            const pessoaAtt = await Pessoas.findByIdAndUpdate(id, dados)
+            //NÃO ESTÁ MOSTRANDO OS DADOS ATUALIZADOS
+            res.render('atualizaPessoa', {
+                pessoaAtt: pessoaAtt
+            })
+        } catch (error) {
+            return res.render('erro', {
+                erro: error.message
+            })
         }
-        const pessoaAtt = await Pessoas.findByIdAndUpdate(id, dados)
-        //NÃO ESTÁ MOSTRANDO OS DADOS ATUALIZADOS
-        res.render('atualizaPessoa', {
-            pessoaAtt: pessoaAtt
-        })
     }
 
     static async deletaPessoaPagina(req, res){
-        const { id } = req.params
-        res.render('deletaPessoa', {
-            pessoaId: id
-        })
+        try {
+            const { id } = req.params
+            return res.render('deletaPessoa', {
+                pessoaId: id
+            })
+        } catch (error) {
+            return res.render('erro', {
+                erro: error.message
+            })
+        }
     }
 
     static async deletaPessoa(req, res){
-        const id = req.body.id
-        await Pessoas.findByIdAndDelete(id.trim(), {$exists: true})
-        return res.redirect('/pessoas')
-        // return res.status(200).send({message: "Usuário deletado com sucesso"})
+        try {
+            const id = req.body.id
+            await Pessoas.findByIdAndDelete(id.trim(), {$exists: true})
+            return res.redirect('/pessoas')
+        } catch (error) {
+            return res.render('erro', {
+                erro: error.message
+            })
+        }
     }
 }
 
